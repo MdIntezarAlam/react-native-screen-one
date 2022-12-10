d1d344633e304225f87a2d410304b29ed8521421
FIRST_NAME LAST_NAME <93022029+MdIntezarAlam@users.noreply.github.com>
2022-12-10 19:13:10 +0530

    screen one task

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+import Packages from './component/Package/Packages';
+
+const App = () => {
+    return (
+        <View style={styles.app_container}>
+            <Header />
+            <Home />
+            <Packages />
+        </View>
+
+
+    );
+}
+
+const styles = StyleSheet.create({
+    app_container: {
+        width: '100vw',
+        height: '290vh',
+        backgroundColor: "#fff",
+    }
+})
+
+export default App;
---
+++ utils/HomeData.js
@@ -0,0 +1,32 @@
+export const homeBox_one = [
+    { name: "Haircut", src: require('../assets/m1.png') },
+    { name: "Makeup", src: require('../assets/m2.jpg') },
+    { name: "Spa", src: require('../assets/m3.png') },
+    { name: "Nails", src: require('../assets/m4.jpg') },
+    { name: "Color", src: require('../assets/m5.png') },
+]
+export const homeBox_two = [
+    { name: "Mia", src: require('../assets/b1.jpg') },
+    { name: "Sandra", src: require('../assets/b3.jpg') },
+    { name: "Jenny", src: require('../assets/b3.jpg') },
+]
+export const packageData = [
+    { heading: "Premium Package", smallTexts: "Premium Package", brSmallText: "Massage + Hair Cut & more...", price: "$ 499", btn: "Buy Now", src: require("../assets/p6.jpg") },
+    { heading: "Standad Package", smallTexts: "Premium Package", brSmallText: "Massage + Hair Cut & more...", price: "$ 249", btn: "Buy Now", src: require("../assets/p6.jpg") },
+]
+export const packageGiftCard = [
+    { src: require("../assets/g1.jpg") },
+    { src: require("../assets/g2.jpg") },
+    { src: require("../assets/g3.jpg") },
+]
+export const packageContact = [
+    { icon: require("../assets/c1.jpg"), conName: "Contact", conNum: "+23678907" },
+    { icon: require("../assets/c2.jpg"), conName: "Email", conNum: "in@dfgjd.com" },
+    { icon: require("../assets/c3.jpg"), conName: "Address", conNum: "589 Westherh Rd.456" },
+]
+
+export const packageGallery = [
+    { src: require("../assets/hd.png") },
+    { src: require("../assets/hd.png") },
+    { src: require("../assets/hd.png") },
+]
